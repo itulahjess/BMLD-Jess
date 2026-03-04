6275b30541fc9a4c98936748c2c10078d0f27974
itulahjess <itulajes@students.zhaw.ch>
2026-03-04 09:24:06 +0100

    changes

--- views/Währungsrechner.py
+++ views/Währungsrechner.py
@@ -6,8 +6,7 @@ st.write("Hier kannst du verschiedene Währungen umrechnen. Gib den Betrag und d
 
 with st.form("currency_form"):
     amount = st.number_input("Betrag", min_value=0.0, step=0.01)
-    from_currency = st.selectbox("Von Währung", ["USD", "EUR", "
-GBP", "JPY"])
+    from_currency = st.selectbox("Von Währung", ["USD", "EUR", "GBP", "JPY"])
     to_currency = st.selectbox("Zu Währung", ["USD", "EUR", "GBP", "JPY"])
     submit_button = st.form_submit_button("Umrechnen") 
 if submit_button:
